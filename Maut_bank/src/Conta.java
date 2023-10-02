3af48c7e3686db903195f1b0ee564d95fab30d28
Marcos Soares <marcos.paulo.s.m.filho@gmail.com>
2023-10-02 17:41:09 -0300

    iniciando projeto

---
+++ Maut_bank/src/Conta.java
@@ -0,0 +1,23 @@
+//Um conta no banco Maut tem número da agência, número da conta, saldo e um Cliente vinculado;
+
+
+public class Conta {
+    private String agencia;
+    private String numero_conta;
+    private double saldo;
+
+    public Conta (String agencia, String numero_conta, double saldo, String id_cliente) {
+        this.agencia = agencia;
+        this.numero_conta = numero_conta;
+        this.saldo = saldo;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+    
+    public double consultarSaldo() {
+        return this.saldo;
+    }
+     
+}
