a3e47cb29e18ac72baadf01823a272a8aa8e1ffc
Marcos Soares <marcos.paulo.s.m.filho@gmail.com>
2023-10-08 16:28:37 -0300

    geracao de numeros automatica

--- Maut_bank/src/Conta.java
+++ Maut_bank/src/Conta.java
@@ -1,8 +1,7 @@
-//Um conta no banco Maut tem número da agência, número da conta, saldo e um Cliente vinculado;
-
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.time.LocalDate;
+import java.util.Random;
 
 public class Conta {
     private String agencia;
@@ -11,6 +10,7 @@ public class Conta {
     private Cliente cliente;
     private List<Receita> receitas;
     private List<Despesa> despesas;
+    private static final String CARACTERES_VALIDOS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     public Conta (String agencia, String numeroConta, double saldo, Cliente cliente) {
         this.agencia = agencia;
@@ -19,6 +19,45 @@ public class Conta {
         this.cliente = cliente;
         this.receitas = new ArrayList<>();
         this.despesas = new ArrayList<>();
+    }
+
+    // Método para gerar um número único de conta
+    private static String gerarNumeroConta() {
+        Random random = new Random();
+        StringBuilder numeroConta = new StringBuilder();
+
+        for (int i = 0; i < 20; i++) {
+            numeroConta.append(CARACTERES_VALIDOS.charAt(random.nextInt(CARACTERES_VALIDOS.length())));
+        }
+
+        // Adiciona um caractere, letra ou dígito ao final
+        numeroConta.append(CARACTERES_VALIDOS.charAt(random.nextInt(CARACTERES_VALIDOS.length())));
+
+        return numeroConta.toString();
+    }
+
+    // Método para gerar um número único de agência
+    private static String gerarNumeroAgencia() {
+        Random random = new Random();
+        StringBuilder numeroAgencia = new StringBuilder();
+
+        for (int i = 0; i < 5; i++) {
+            numeroAgencia.append(CARACTERES_VALIDOS.charAt(random.nextInt(CARACTERES_VALIDOS.length())));
+        }
+
+        // Adiciona um caractere, letra ou dígito ao final
+        numeroAgencia.append(CARACTERES_VALIDOS.charAt(random.nextInt(CARACTERES_VALIDOS.length())));
+
+        return numeroAgencia.toString();
+    }
+
+    // Método para criar uma nova conta com números únicos
+    public static Conta setConta(Cliente cliente) {
+        String numeroAgencia = gerarNumeroAgencia();
+        String numeroConta = gerarNumeroConta();
+        double saldoInicial = 0.0; 
+
+        return new Conta(numeroAgencia, numeroConta, saldoInicial, cliente);
     }
 
     public String getAgencia() {
@@ -40,7 +79,7 @@ public class Conta {
     public void depositar (double valor, String descricao) {
         if (valor > 0) {
             this.saldo += valor;
-            Receita transacao = new Receita(new Date(), descricao, valor);
+            Receita transacao = new Receita(descricao, valor);
             this.receitas.add(transacao);
         } else {
             System.out.println("Valor de depósito inválido.");
@@ -50,7 +89,7 @@ public class Conta {
     public void sacar (double valor, String descricao) {
         if (valor > 0) {
             this.saldo += valor;
-            Despesa transacao = new Despesa (new Date(), descricao, valor);
+            Despesa transacao = new Despesa (descricao, valor);
             this.despesas.add(transacao);
         } else {
             System.out.println("Valor de saque inválido ou saldo insuficiente.");
@@ -60,7 +99,7 @@ public class Conta {
     public void transferir ( double valor, Conta contaDestino, String descricao){
         if(valor > 0 && this.saldo >= valor){
             this.saldo -= valor;
-            Despesa transacao = new Despesa(new Date(), descricao, valor);
+            Despesa transacao = new Despesa(descricao, valor);
             this.despesas.add(transacao);
             // contaDestino.despositar(valor, descricao);
         }else{
